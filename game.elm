58c313538a874ff16863cc775d09e44fa7be1684
Cheezmeister <@gmail>
2015-12-13 17:23:10 -0800

    Growing

--- game.elm
+++ game.elm
@@ -6,21 +6,35 @@ import Keyboard
 import Time exposing (..)
 import Window
 import String
-import List
+import List exposing (map)
+import Random exposing (Seed, generate, initialSeed)
+import Maybe exposing (withDefault)
 
 
 -- MODEL
 
-areaW = 400
-areaH = 300
+areaW = 800
+areaH = 600
+
+bottom = -areaH/2 + 28
 
 type alias Debuggable = Float
 
 type alias Input = (Time, { x:Int, y:Int }, Bool)
 
+type ThingKind = Grow | Shrink
+type alias Thing = 
+  { x : Float
+  , y : Float
+  , vx : Float
+  , vy : Float
+  , kind : ThingKind 
+  }
+
 type alias Model =
   { hero : Player
-  -- , stuff : List Thing
+  , stuff : List Thing
+  , ticks : Float
   }
 
 type alias Player =
@@ -28,15 +42,15 @@ type alias Player =
   , y : Float
   , vx : Float
   , vy : Float
-  , dir : String
+  , size : Float
   }
 
 
 hero : Player
 hero =
-  Player 0 0 0 0 "south"
+  Player 0 0 0 0 1.0
 
-model = Model hero
+model = Model hero [] 0
 
 sign : Float -> Float
 sign n =
@@ -49,21 +63,57 @@ sign n =
 
 -- UPDATE
 
-update : (Time, { x:Int, y:Int }, Bool) -> Model -> Model
+update : Input -> Model -> Model
 update (timeDelta, direction, isRunning) model =
-  Model (updatePlayer (timeDelta, direction, isRunning) model.hero) -- (Player model.hero.x+1 model.hero.y model.hero.vx model.hero.vy model.hero.dir)
-  
-  
+  let 
+      newplayer = updatePlayer (timeDelta, direction, isRunning) model.hero
+      newstuff = model.stuff
+        |> spawnStuff (model.ticks, timeDelta)
+        |> moveStuff (timeDelta)
+        |> List.filter (isOnScreen newplayer)
+  in
+    Model newplayer newstuff (model.ticks + Time.inMilliseconds timeDelta)
+        |> collide
+  -- TODO why this no work
   -- { model |
   --     hero = updatePlayer (timeDelta, direction, isRunning) hero
   -- }
 
 
-updatePlayer : (Time, { x:Int, y:Int }, Bool) -> Player -> Player
+spawnStuff : (Float, Time) -> List Thing -> List Thing
+spawnStuff (ticks, timeDelta) stuff =
+  let
+    (roll, seed) = generate (Random.float 0 100) (initialSeed (round ticks))
+  in
+    if roll < timeDelta then
+      spawnThing seed :: stuff
+    else
+      stuff
+
+
+spawnThing : Seed -> Thing 
+spawnThing seed =
+  let
+      (x, seed2) = generate (Random.float (-areaW/2) (areaW/2)) seed
+      (vx, seed3) = generate (Random.float -5 5) seed2
+      (vy, seed4) = generate (Random.float 0 5) seed3
+  in
+    Thing x (areaH*1.5) vx vy Grow
+
+
+moveStuff : (Time) -> List Thing -> List Thing
+moveStuff (timeDelta) stuff =
+  stuff |> map (\thing -> 
+    { thing |
+      y = thing.y - thing.vy * timeDelta
+    }
+  )
+
+
+updatePlayer : Input -> Player -> Player
 updatePlayer (timeDelta, direction, isRunning) hero =
   hero
     |> newVelocity isRunning direction
-    |> setDirection direction
     |> updatePosition timeDelta
 
 
@@ -90,13 +140,6 @@ newVelocity isRunning {x,y} model =
       }
 
 
-setDirection : { x:Int, y:Int } -> Player -> Player
-setDirection {x,y} model =
-  { model |
-      dir = "west"
-  }
-
-
 updatePosition : Time -> Player -> Player
 updatePosition dt ({x,y,vx,vy} as model) =
   { model |
@@ -105,9 +148,37 @@ updatePosition dt ({x,y,vx,vy} as model) =
   }
 
 
+isOnScreen player entity =
+  entity.y > -areaW/2
+
+
+collide : Model -> Model
+collide model =
+  let
+    overlap player thing =
+      (abs (thing.x - player.x) < player.size*11) && (abs (thing.y - bottom) < player.size*14)
+    collision player stuff = 
+      List.any (overlap player) stuff
+    handleCollision player stuff =
+      if collision model.hero model.stuff then 
+        (\h -> {h | size = h.size*1.5}) 
+      else 
+        (\h -> h)
+
+    grow = handleCollision model.hero model.stuff
+    newstuff = model.stuff |> List.filter (not << overlap model.hero)
+  in
+    { model |
+         hero = grow model.hero
+       , stuff = newstuff
+    }
+
+
+-- VIEW
+
 debugexpr : (String, Debuggable) -> String
 debugexpr (name, expr) =
-  name ++ "=" ++ toString (round expr)
+  name ++ "=" ++ toString (expr)
 
 debug : List (String, Debuggable) -> Form
 debug vars =
@@ -117,33 +188,41 @@ debug vars =
     |> toForm
     |> move (0, areaH/2 - 16)
 
--- VIEW
-
 view : (Int,Int) -> Model -> Element
 view (w,h) model =
   let
     verb = if vx == 0 && vy == 0 then "stand" else "walk"
     src = "/imgs/UFO.gif"
     offset = toFloat -(round y % areaH)
-    {x,y,vx,vy,dir} = model.hero
+    {x,y,vx,vy,size} = model.hero
   in
     container w h middle <|
-    collage areaW areaH
+    collage areaW areaH <|
+      -- Background
       [ toForm (image areaW areaH "/imgs/desert.png")
          |> move (0, offset)
       , toForm (image areaW areaH "/imgs/desert.png")
          |> move (0, areaH + offset)
-      , (toForm (image 22 28 src))
-          |> rotate (degrees (vx * -5))
-          |> move (x, -areaH/2 + 28)
-      , debug [("y", y), ("x", x), ("dt", vy)]
 
-      -- , "y: " ++ toString (round y) ++ ",x: " ++ toString (round x)
-      --    |> toForm << show
-      --    |> move (0, areaH/2 - 16)
-      -- , toForm (Markdown.toElement "Arrows to move<br/>Shift to run")
-      --     |> move (70-areaW/2, 30-areaH/2)
-      ]
+      -- Player
+      , (toForm (image 22 28 src))
+          |> scale size
+          |> rotate (degrees (vx * -5))
+          |> move (x, bottom)
+
+      -- , debug [("x", model.stuff |> map .x |> List.head |> Maybe.withDefault 0)]
+      , debug [("y", y)]
+
+      ] ++ (drawStuff model.stuff)
+
+drawStuff : List Thing -> List Form
+drawStuff stuff =
+  stuff |> List.map drawThing
+
+drawThing : Thing -> Form
+drawThing {x, y, kind} =
+  toForm (image 32 32 "/imgs/plus.png")
+    |> move (x, y)
 
 
 -- SIGNALS
